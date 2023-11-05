45708d46b3e12bed15c52b6e2fbcb68012cf5f3a
RaulStanciu02 <raul.stanciu02@e-uvt.ro>
2023-11-05 17:13:03 +0200

    Initial Commit

---
+++ src/main/java/com/reviewhub/ReviewHubApplication.java
@@ -0,0 +1,13 @@
+package com.reviewhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReviewHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewHubApplication.class, args);
+    }
+
+}
