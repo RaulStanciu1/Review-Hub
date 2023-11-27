5ef5372fc41e969ad1b6e8c8511a5dcf1dbedd48
nuta <nuta_bogda@yahoo.com>
2023-11-27 14:02:12 +0200

    Initial spring commit

--- src/main/java/com/reviewhub/Application.java
+++ src/main/java/com/reviewhub/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ReviewHubApplication {
+public class Application {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ReviewHubApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
 
 }
