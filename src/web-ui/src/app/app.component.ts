d18d2354f8f4ad6ea286242b58c6074174b40952
Tugui Andrei <andrei.tugui02@e-uvt.ro>
2023-12-05 13:52:13 +0200

    added components

--- src/web-ui/src/app/app.component.ts
+++ src/web-ui/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,7 +9,31 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'web-ui';
   r_username = "RaulStanciu";
+  ldm_mode = localStorage.getItem("ldm_mode");
+  app_default = localStorage.getItem("app_default");
+
+  ldmClick() {
+    if(this.ldm_mode == "0") {
+      this.ldm_mode = "1";
+    } else {
+      this.ldm_mode = "0"
+    }
+
+    localStorage.setItem("ldm_mode", this.ldm_mode);
+  }
+  
+  ngOnInit() {
+    if(this.app_default == "true") {
+      console.log("App: Default is set!")
+    } else {
+      localStorage.setItem("ldm_mode", "0");
+      this.ldm_mode = localStorage.getItem("ldm_mode");
+      localStorage.setItem("app_default", "true");
+      this.app_default = localStorage.getItem("app_default");
+      console.log("App: Default was setup!")
+    }
+  }
 }
