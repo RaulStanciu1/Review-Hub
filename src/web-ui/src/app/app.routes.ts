6837f9d298c7456b6bfa5cdff0e33730a7113a02
Tugui Andrei <andrei.tugui02@e-uvt.ro>
2023-12-05 13:52:13 +0200

    added components

--- src/web-ui/src/app/app.routes.ts
+++ src/web-ui/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ReviewsComponent } from './reviews/reviews.component';
+import { TeamsComponent } from './teams/teams.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: WelcomeComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'teams', component: TeamsComponent },
+    { path: 'reviews', component: ReviewsComponent }
+];
